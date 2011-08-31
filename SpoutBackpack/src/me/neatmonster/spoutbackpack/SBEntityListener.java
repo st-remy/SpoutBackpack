d45946c6992e3d1c1aa77356e6783b0af2fc9e63
Alexandre ADAMSKI <neatmonster@hotmail.fr>
2011-08-31 10:36:42 +0200

    Version 2.2.1.
    Signed-off-by: Alexandre ADAMSKI <neatmonster@hotmail.fr>

--- SpoutBackpack/src/me/neatmonster/spoutbackpack/SBEntityListener.java
+++ SpoutBackpack/src/me/neatmonster/spoutbackpack/SBEntityListener.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityListener;
 import org.bukkit.inventory.ItemStack;
+import org.getspout.spout.inventory.CustomInventory;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class SBEntityListener extends EntityListener {
@@ -28,17 +29,27 @@ public class SBEntityListener extends EntityListener {
 				if (plugin.inventories.containsKey(player.getName())) {
 					ItemStack[] items = plugin.inventories
 							.get(player.getName());
-					plugin.inventories.remove(player.getName());
 					for (ItemStack item : items) {
 						if (item != null && item.getAmount() > 0) {
 							player.getWorld().dropItem(player.getLocation(),
 									item);
 						}
 					}
-				}
-				if (!((SpoutPlayer) player).isSpoutCraftEnabled()) {
+					CustomInventory inventory = new CustomInventory(
+							plugin.inventoriesSize.get(player.getName()),
+							plugin.inventoryName);
+					for (Integer i = 0; i < plugin.inventoriesSize.get(player
+							.getName()); i++) {
+						ItemStack item = new ItemStack(0, 0);
+						inventory.setItem(i, item);
+					}
+					plugin.inventories.put(player.getName(),
+							inventory.getContents());
 					SBInventorySaveTask
 							.saveInventory(player, player.getWorld());
+				}
+				if (!((SpoutPlayer) player).isSpoutCraftEnabled()) {
+
 					player.sendMessage(plugin.logTag
 							+ " Your "
 							+ ChatColor.RED
